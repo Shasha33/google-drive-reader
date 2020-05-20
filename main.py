2677d55f321b569bda0a120e971dca7cdcc2c0b0
Michael <michael.nik999@gmail.com>
2020-05-20 17:52:22 +0300

    fix auth

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import urllib.parse
 import json
 
 params = urllib.parse.urlencode(
-    {'@client_id': '480236582879-8h0b7vfns2lmou9ot9f3b3ivl2a72eg1.apps.googleusercontent>', '@response_typ': 'code', '@scope': 'https://www.googleapis.com/auth/drive.readonly', '@redirect_url': 'urn:ietf:wg:oauth:2.0:oob' }
+    {'client_id': '480236582879-8h0b7vfns2lmou9ot9f3b3ivl2a72eg1.apps.googleusercontent.com', 'response_type': 'code', 'scope': 'https://www.googleapis.com/auth/drive.readonly', 'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob' }
 )
 
-
+print(params)
 c = http.client.HTTPSConnection("accounts.google.com")
 
 headers = {"Authorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l": "Basic YWxhZGRpbjpvcGVuc2VzYW1l"}
-c.request("POST", "/o/oauth2/v2/auth", params, {})
+c.request("POST", "o/oauth2/v2/auth", params, {})
 response = c.getresponse()
 print(response.status, response.reason)
 print(response.read().decode())

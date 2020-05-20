e002161c91535a789a9b4e18f6362a783981c52c
Darya Razumova <d.razumova@yandex.ru>
2020-05-20 16:47:41 +0300

    add example

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import http.client
+import urllib.parse
 
-c = http.client.HTTPSConnection("www.googleapis.com")
+params = urllib.parse.urlencode(
+    {'@client_id': '480236582879-8h0b7vfns2lmou9ot9f3b3ivl2a72eg1.apps.googleusercontent>', '@response_typ': 'code', '@scope': 'https://www.googleapis.com/auth/drive.metadata.readonly', '@redirect_ur': 'urn:ietf:wg:oauth:2.0:oob' }
+)
 
-headers = {"key": "password_key"}
-c.request("POST", "/auth/documents.readonly", "", headers)
+
+c = http.client.HTTPSConnection("accounts.google.com")
+
+headers = {"Authorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l": "Basic YWxhZGRpbjpvcGVuc2VzYW1l"}
+c.request("POST", "/o/oauth2/v2/auth", params, {})
 response = c.getresponse()
 print(response.status, response.reason)
+print(response)
